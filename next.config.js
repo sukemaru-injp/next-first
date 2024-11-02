b23360c43a1bcfd88e6d82ae62de84dfbdde13b2
RyosukeKubota <sukemaro_0@yahoo.co.jp>
2024-11-02 16:22:34 +0900

    fix/vanilla

---
+++ next.config.js
@@ -0,0 +1,34 @@
+const {
+  createVanillaExtractPlugin
+} = require('@vanilla-extract/next-plugin');
+
+const withVanillaExtract = createVanillaExtractPlugin();
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    MICROCMS_API_KEY: process.env.MICROCMS_API_KEY,
+    MICROCMS_SERVICE_DOMAIN: process.env.MICROCMS_SERVICE_DOMAIN
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production'
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/profile',
+        permanent: true,
+      },
+    ]
+  },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.microcms-assets.io' },
+    ]
+  },
+}
+
+module.exports = withVanillaExtract(nextConfig);
